67c978cee64821ea417adbad10643dd7a43833bc
Douglas <douglasdothnc@gmail.com>
2022-10-28 19:38:10 +0100

    [DEV] Registration
    Create a class that describe the user registration request.
    Created a service that create a new User and pass to the signup function in user service.
    Created a controller (API) for user to access.

---
+++ src/main/java/Clips/backend/registration/RegistrationController.java
@@ -0,0 +1,36 @@
+package Clips.backend.registration;
+
+import Clips.backend.response.Response;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static java.time.LocalDateTime.now;
+import static java.util.Map.of;
+import static org.springframework.http.HttpStatus.CREATED;
+
+// RESTful API layer to register a user.
+// e.g.: http://localhost:8080/api/v1/registration
+@RestController
+@RequestMapping(path = "api/v1/registration")
+@AllArgsConstructor
+public class RegistrationController {
+    private RegistrationService registrationService;
+
+    @PostMapping
+    public ResponseEntity<Response> register(@RequestBody RegistrationRequest request) {
+        // Get a json from frontend and register the user using the registration service
+        return ResponseEntity.ok(
+            Response.builder()
+                .timeStamp(now())
+                .data(of("response", registrationService.register(request)))
+                .message("[RegistrationController|register] Registration request sent.")
+                .httpStatus(CREATED)
+                .statusCode(CREATED.value())
+                .build()
+        );
+    }
+}
---
+++ src/main/java/Clips/backend/registration/RegistrationService.java
@@ -0,0 +1,30 @@
+package Clips.backend.registration;
+
+import Clips.backend.user.User;
+import Clips.backend.user.UserRole;
+import Clips.backend.user.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class RegistrationService {
+
+    // Services injection.
+    private final UserService userService;
+
+    // Register a user.
+    public User register(RegistrationRequest request) {
+        // Email and password already validated in frontend.
+        return userService.signUpUser(
+            new User(
+                request.getName(),
+                request.getEmail(),
+                request.getPassword(),
+                request.getAge(),
+                request.getPhoneNumber(),
+                UserRole.USER
+            )
+        );
+    }
+}
