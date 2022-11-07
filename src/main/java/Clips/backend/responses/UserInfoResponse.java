fc6d008ccabcbfb6c2395f9ce919c95f36e93156
Douglas <douglasdothnc@gmail.com>
2022-11-07 19:04:01 +0000

    [DEV] Add responses
    Add two more type of responses for user info and messages.

---
+++ src/main/java/Clips/backend/responses/UserInfoResponse.java
@@ -0,0 +1,16 @@
+package Clips.backend.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserInfoResponse {
+    private String name;
+    private String email;
+    private List<String> roles;
+}
